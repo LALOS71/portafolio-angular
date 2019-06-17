5cef992b561f2a217b74d64a5e9ca54226fc7b2b
Dardo Lopez <Dardolop@gmail.com>
2019-06-17 17:39:42 -0300

    Fin Seccion 4

--- src/app/interfaces/info-pagina.interface.ts
+++ src/app/interfaces/info-pagina.interface.ts
@@ -1,15 +1,13 @@
 
-
-
-interface InfoPagina {
-  titulo?: string;
-  email?: string;
-  nombre?: string;
-  pagina_autor?: string;
-  facebook?: string;
-  twitter?: string;
-  instagram?: string;
-  equipo_trabajo?: any[];
+export interface InfoPagina {
+    titulo?: string;
+    email?: string;
+    nombre?: string;
+    pagina_autor?: string;
+    facebook?: string;
+    twitter?: string;
+    instagram?: string;
+    equipo_trabajo?: any[];
 }
 
 
--- src/app/services/info-pagina.service.ts
+++ src/app/services/info-pagina.service.ts
@@ -2,6 +2,7 @@ import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
 import { InfoPagina } from '../interfaces/info-pagina.interface';
 
+
 @Injectable({
   providedIn: 'root'
 })
@@ -16,7 +17,7 @@ export class InfoPaginaService {
 
     //Leer el archivo JSON
     this.http.get('assets/data/data-pagina.json')
-        .subscribe( resp => {
+        .subscribe( (resp: InfoPagina) => {
 
           this.cargada = true;
           this.info = resp;
