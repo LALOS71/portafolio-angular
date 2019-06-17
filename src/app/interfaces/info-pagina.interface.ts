f9b64a8849365bb3e8056f823181ae264e9659b7
Dardo Lopez <Dardolop@gmail.com>
2019-06-17 08:44:34 -0300

    template listo

---
+++ src/app/interfaces/info-pagina.interface.ts
@@ -0,0 +1,15 @@
+
+
+
+interface InfoPagina {
+  titulo?: string;
+  email?: string;
+  nombre?: string;
+  pagina_autor?: string;
+  facebook?: string;
+  twitter?: string;
+  instagram?: string;
+  equipo_trabajo?: any[];
+}
+
+
