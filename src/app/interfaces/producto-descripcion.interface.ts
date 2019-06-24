f3599409cfc51aecdae6e5e499c255e061c1861b
Dardo Lopez <Dardolop@gmail.com>
2019-06-23 23:06:27 -0300

    fin seccion 7

---
+++ src/app/interfaces/producto-descripcion.interface.ts
@@ -0,0 +1,15 @@
+import { Producto } from './producto.interface';
+
+export interface ProductoDescripcion {
+    categoria: string;
+    desc1: string;
+    desc2: string;
+    producto: string;
+    resumen: string;
+    subtitulo1: string;
+    subtitulo2: string;
+    
+}
+
+
+
--- src/app/services/productos.service.ts
+++ src/app/services/productos.service.ts
@@ -1,6 +1,9 @@
 import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
 import { Producto } from '../interfaces/producto.interface';
+// import { ConsoleReporter } from 'jasmine';
+// import { Promise } from 'q';
+// import { resolve } from 'url';
 
 
 @Injectable({
@@ -10,6 +13,7 @@ export class ProductosService {
 
   cargando = true;
   productos: Producto[] = [];
+  productosfiltrado: Producto[] = [];
 
   constructor( private http: HttpClient ) { 
 
@@ -17,21 +21,63 @@ export class ProductosService {
 
   }
 
-
   private cargarProductos() {
 
-    this.http.get("https://angular-html-1414e.firebaseio.com/productos_idx.json")
-    .subscribe( (resp: Producto[]) => {
-
-      console.log(resp);
-      this.productos = resp;
+    return new Promise( ( resolve, reject ) => {
       
-      setTimeout(() => {
+      this.http.get("https://angular-html-1414e.firebaseio.com/productos_idx.json")
+          .subscribe( (resp: Producto[]) => {
+        this.productos = resp;
         this.cargando = false;
+        resolve();
         
-      }, 2000);
-      
+        // setTimeout(() => {
+        //   this.cargando = false;
+          
+        // }, 2000);
+       });
     });
+
   }
 
+    getProducto ( id: string ) {
+
+      return this.http.get(`https://angular-html-1414e.firebaseio.com/productos/${ id }.json`)
+    }
+
+    buscarProducto( termino: string ) {
+
+      if ( this.productos.length === 0 ) {
+        //cargar productos
+        this.cargarProductos().then( ( ) => {
+          //despues de tener los productos
+          //Aplicar filtro
+          this.filtrarProductos( termino );
+        });
+      } else {
+        //aplicar el filtro
+        this.filtrarProductos( termino );
+      }
+
+       
+    }
+    
+    private filtrarProductos( termino: string ) {
+      
+      //console.log(this.productos);
+      this.productosfiltrado = [];
+
+      termino = termino.toLowerCase();
+
+      this.productos.forEach( prod => {
+
+        const tituloLower = prod.titulo.toLowerCase();
+
+        if ( prod.categoria.indexOf( termino ) >= 0 || tituloLower.indexOf( termino ) >= 0) {
+          this.productosfiltrado.push( prod );
+        }
+
+      });
+
+    }
 }
